239287f7685bb064e37451aa8d0af3991ee3ae04
Salah ammour <salahamm4year@gmail.com>
2024-05-09 17:39:03 +0100

    Add Clear and Add button

--- project.js
+++ project.js
@@ -3,31 +3,37 @@ const container = document.createElement('div');
 
 container.setAttribute("id" , "container");// --2 - 1=2
 
+
 const body = document.querySelector('body');
 
-body.appendChild(createDiv());
-
+body.appendChild(changeSize());
+//create button to interact with the sketch
 const button = document.createElement('button')
-
-
-
-const input = document.createElement('input')
-input.setAttribute("class" , "input")
-
+const popup = document.createElement('button')
+popup.setAttribute("class" , "popup")
 button.textContent = "Clear the Sketch"
-input.textContent = "Change the size of the sketch "
+popup.textContent = "Click to Change the size of the sketch "
 
 body.appendChild(button);
-body.appendChild(input);
-
-const mouseover = hover();
-// const clear = clearButton(button);
+body.appendChild(popup);
 
 
+// const mouseover = hover();
+
+function input () {
+    prompt("enter the the size of the sketch");
+}
+
+
+
+popup.addEventListener('click' , () => {
+    let input = input();
+    changeSize (input);
+} )
 
 
 //create 16x16 grid square
-    function createDiv () {
+    function firstDiv () {
         let content = container  
         for (i = 0 ; i < 16 * 16 ; i++) {
             const square = document.createElement('div'); 
@@ -60,14 +66,31 @@ button.addEventListener ('click' , () => {
     })
 })
 
-//input the changes
-input.addEventListener('click' , () => {
-    if (input.textContent > +"100" && input.textContent < +"1")  {
-        input.textContent = "";
+
+
+function changeSize (input) {
+    let number = parseInt(input);
+    let content = container ;
+    if ( number === 0 || number > 100 || number === '' ) {
+        prompt("enter the the size of the sketch");
     }else {
-        
+        for ( let i = 0 ; i < number * number ; i++){
+            const square = document.createElement('div'); 
+            square.setAttribute("class" , "square");
+            content.style.width = "50%"
+            content.style.height = "50%"
+            square.style.width = `calc(100% / ${number})`;
+            square.style.height = `calc(100% / ${number})`;
+            content.appendChild(square);
+            
+
+        }
+
+        hover()
+
+        return content;
     }
-}) 
+}
 
 
 
