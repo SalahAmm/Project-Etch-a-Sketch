ed9e2b67e7d7f690f7bb2db3083ae7b2cb1de523
Salah ammour <salahamm4year@gmail.com>
2024-05-09 21:21:16 +0100

    Complete the project

--- project.js
+++ project.js
@@ -1,111 +1,83 @@
-// create container element 
-const container = document.createElement('div');
+// create container element
+const container = document.createElement("div");
 
-container.setAttribute("id" , "container");// --2 - 1=2
+container.setAttribute("id", "container"); // --2 - 1=2
 
+const body = document.querySelector("body");
 
-const body = document.querySelector('body');
-
-body.appendChild(changeSize());
+body.appendChild(container);
 //create button to interact with the sketch
-const button = document.createElement('button')
-const popup = document.createElement('button')
-popup.setAttribute("class" , "popup")
-button.textContent = "Clear the Sketch"
-popup.textContent = "Click to Change the size of the sketch "
+const button = document.createElement("button");
+const popup = document.createElement("button");
+popup.setAttribute("class", "popup");
+button.textContent = "Clear the Sketch";
+popup.textContent = "Click to Change the size of the sketch ";
 
 body.appendChild(button);
 body.appendChild(popup);
 
-
 // const mouseover = hover();
 
-function input () {
-    prompt("enter the the size of the sketch");
-}
+popup.addEventListener("click", () => {
+    const inputValue = prompt("enter a value between 1 to 99");
+    changeSize(inputValue);
+});
 
-
-
-popup.addEventListener('click' , () => {
-     
-    changeSize (input());
-} )
-
-
-//create 16x16 grid square
-    function firstDiv () {
-        let content = container  
-        for (i = 0 ; i < 16 * 16 ; i++) {
-            const square = document.createElement('div'); 
-            square.setAttribute("class" , "square");
-            content.appendChild(square);
-         }
-         return content;
-
-
-
-    }
-
-
-//make the div change color when hover
-
-    const square = document.querySelectorAll('.square');;
-    square.forEach((square) => {
-        square.addEventListener("mouseover" , () => {
-            square.style.backgroundColor = "black";
-        }
-    }    
-//clear the container 
-
-button.addEventListener ('click' , () => {
-    const clear = document.querySelectorAll('.square');
+button.addEventListener("click", () => {
+    const clear = document.querySelectorAll(".square");
     clear.forEach((square) => {
         square.style.backgroundColor = null;
-    })
-})
+    });
+});
 
-createGrid()
-
-function createGrid(number = 16){
+createGrid(16);
 
 
-    for ( let i = 0 ; i < number * number ; i++){
-        const square = document.createElement('div'); 
-        square.setAttribute("class" , "square");
-        content.style.width = "50%"
-        content.style.height = "50%"
+//clear the container
+
+function createGrid(number) {
+    let content = container;
+    for (let i = 0; i < number * number; i++) {
+
+        
+        const square = document.createElement("div");
+
+        square.setAttribute("class", "square");
+
         square.style.width = `calc(100% / ${number})`;
         square.style.height = `calc(100% / ${number})`;
         content.appendChild(square);
-        
 
+
+        square.addEventListener("mouseover", () => {
+            let red = Math.floor(Math.random() * 255);
+            let green = Math.floor(Math.random() * 255);
+            let blue = Math.floor(Math.random() * 255);
+            square.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        });
     }
+
+    return content;
 }
 
-
-function changeSize (input) {
+function changeSize(input) {
     let number = parseInt(input);
-    let content = container ;
-    if ( number === null || number > 100 || number === '' ) {
+    let content = container;
+
+    if (number === null || number > 100 || number < 1) {
         prompt("Error");
-        createGrid(16)
-
-    }else {
-        createGrid(number)
-            
-
-        }
-
-        hover()
-
+        content = createGrid(16);
+        return content;
+    } else {
+        removeAllChildNodes(content);
+        content = createGrid(number);
         return content;
     }
 }
 
 
-
-
-
-
-
-
+ function removeAllChildNodes (container) {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
